8eff304f579d8197a8bf39c925043ad9c48a34af
student-a210 <student-a210@PC-A210-7>
2024-05-04 10:43:27 +0500

    Разработаны классы. Нецветаев

---
+++ Models/Kinoteatr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика_27.Models
+{
+    public class Kinoteatr
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public int CountZal { get; set; }
+        public int Count { get; set; }
+
+        public Kinoteatr(int id, string Name, int CountZal, int Count)
+        {
+            this.id = id;
+            this.Name = Name;
+            this.CountZal = CountZal;
+            this.Count = Count;
+        }
+    }
+}
